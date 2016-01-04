0961f0d0fb37fbb66e94e0b35c0241eef0c4862c
Shawn Watson <shawnw86@gmail.com>
2016-01-04 13:42:15 -0800

    Add working advanced search for everything except mechanics

--- app/helpers/search.rb
+++ app/helpers/search.rb
@@ -3,17 +3,14 @@ class Search
   def initialize(params)
     @cards = JSON.parse(File.read('./public/cards.json'))
     @search_type = params['searchType']
-    @search_term = params['searchTerm']
-    @rarity = params['rarity']
-    @type = params['type']
-    @mechanic = params['mechanic']
-    @cost = params['cost']
-    @health = params['health'].to_i if params['health'].length > 0 
-    @attack = params['attack'].to_i if params['attack'].length > 0
-    p params['health'].length
-    p @attack
-    p @health
     @results = []
+    @search_term = params['searchTerm']
+    @rarity = params['rarity'].downcase
+    @type = params['type'].downcase
+    @mechanic = params['mechanic'].downcase unless params['mechanic'] = ""
+    @cost = params['cost'].to_i unless params['cost'] == ""
+    @health = params['health'].to_i unless params['health'] == ""
+    @attack = params['attack'].to_i unless params['attack'] == ""
   end
 
   def find_card
@@ -30,7 +27,11 @@ class Search
 
   def advanced_search
     @cards.each do |card|
-      @results << card if card['attack'] == @attack && card['health'] == @health
+      @results << card if (card['attack'] == @attack || @attack == nil) \
+                      && (card['health'] == @health || @health == nil) \
+                      && (card['cost'] == @cost || @cost == nil) \
+                      && (card['rarity'].downcase == @rarity || @rarity == 'all') \
+                      && (card['type'].downcase == @type || @type == 'all' )
     end
     @results
   end
